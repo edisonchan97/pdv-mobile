0cd431cbe9b31223ba564448d5c8f5858bdc9e3a
liviuscn <1216369595@qq.com>
2018-05-05 00:38:59 +0800

    修改

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import home from '@/apps/home/reducer';
-import production from '@/apps/production/reducer';
+import formData from '@/apps/home/reducer';
+import proData from '@/apps/production/reducer';
 //创建app后将app中的reducer增加
 const rootReducer = combineReducers({
-    formData:home,
-    proData:production
+    formData,
+    proData
 });
 
 export default rootReducer
