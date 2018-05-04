846f7ad515e54fcd86311d62d51fe95272f3c7be
liviuscn <1216369595@qq.com>
2018-05-04 13:12:28 +0800

    修改

--- src/reducers.js
+++ src/reducers.js
@@ -1,21 +1,10 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
-import todoList from './todoList';
-import message from './message';
-import product from './product';
-import detail from './detail';
-import shopcar from './shopcar';
-import user from './user';
-
-//--将pages中的reducer
+import home from '@/apps/home/reducer';
+import production from '@/apps/production/reducer';
+//创建app后将app中的reducer增加
 const rootReducer = combineReducers({
-    counter,
-    todoList,
-    message,
-    product,
-    detail,
-    shopcar,
-    user
+    home,
+    production
 });
 
 export default rootReducer
