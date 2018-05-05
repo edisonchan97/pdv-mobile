a7ab3062066a258096f5a90cf53d54d1bdb68938
liviuscn <1216369595@qq.com>
2018-05-05 23:48:11 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import FastClick from 'fastclick';
-import configureStore from './store';
-import Routes from './routers';
-import './utils/setRem';
-import './style/base.css';
-import registerServiceWorker from './registerServiceWorker';
+import configureStore from '@/store';
+import Routes from '@/routers';
+import '@/utils/setRem';
+import '@/style/base.css';
+import registerServiceWorker from '@/registerServiceWorker';
 
 const store = configureStore();
 
