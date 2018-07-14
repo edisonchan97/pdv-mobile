398bc3e61d8f76cac48bb26f658a2b4267ee6d39
liviuscn <1216369595@qq.com>
2018-07-14 23:52:12 +0800

    git push

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -103,7 +103,7 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       '@': resolve('src'),//--为了以后import组件方便，将src目录取别名为@
-      'pdv':resolve('src/components/index.js')
+      'pdv':resolve('src/components')
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
